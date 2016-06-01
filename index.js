e8c33280e98abed4b354cc596c3636a708580b43
Jon Horner <hornerjn@mail.uc.edu>
2016-06-01 10:19:02 -0400

    Updated package.json
    * Changed `author` field to `contributors`
    * Set `private` to `true`
    * Referenced the license
    
    Also moved the license from `./index.js` to `./LICENSE.txt`

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-/**
- * Copyright (C) 2016 THRIVE - All Rights Reserved
- * Unauthorized copying and/or usage of this file, via any medium is strictly prohibited
- * Proprietary and confidential
- *
- * Jon Horner <jon.horner@ingagepartners.com>, May 2016
- */
-
 require('./styles/index.less');
