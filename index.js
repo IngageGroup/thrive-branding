5f8ab1f4ba00bc017a9ef5fcd3c4d3fd5beb3340
Jon Horner <hornerjn@mail.uc.edu>
2016-06-06 13:31:48 -0400

    Added hrefs to dropdowns

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
 require('./styles/index.less');
 
 var Thrive = require('./src/thrive.js');
-
-console.log('index.js');
--- src/thrive.js
+++ src/thrive.js
@@ -1,79 +1,83 @@
-import { keys } from 'ramda';
+import { isEmpty, keys } from 'ramda';
 
 const navStruct = {
-  'news': [
-    'News',
-    'Events',
-    'Thrive Afternoons',
-    'B Corp'
-  ],
-  'employeegrowth': [
-    'Coaching',
-    'Grad Plan',
-    'Consultative Competencies',
-    'Buddy Program',
-    'Learning Portal'
-  ],
-  'hub': [
-    'Recruiting',
-    'Benefits',
-    'New Hires',
-    'Quickbooks',
-    'Paycor',
-    'Policies & Procedures',
-    'Performance Reviews',
-    'Employee Directory'
-  ],
-  'projects': [
-    'Client Projects',
-    'Client Project Archives',
-    'Internal Projects',
-    'Internal Project Archives'
-  ],
-  'sales': [
-    'Public Website',
-    'Marketing Collateral',
-    'Client Presentations',
-    'Case Studies',
-    'Ripple Report & Culture Book',
-    'Business Development',
-    'Salesforce'
-  ],
-  'operations': [
-    'I.T.',
-    'Building/Real Estate',
-    'Quickbooks',
-    'Expense Reports',
-    'Utilization Report',
-    'Finance Resources'
-  ],
-  'forum': [
+  'news': {
+    'News': 'news',
+    'Events': 'events',
+    'Thrive Afternoons': 'thriveafternoons',
+    'B Corp': 'b_corp',
+  },
+  'employeegrowth': {
+    'Coaching': 'coaching',
+    'Grad Plan': 'gradplan',
+    'Consultative Competencies': 'consultativecompetencies',
+    'Buddy Program': 'buddyprogram',
+    'Learning Portal': 'learningportal',
+  },
+  
+  // TODO: add paths to the following dropdown items
+  'hub': {
+    'Recruiting': '',
+    'Benefits': '',
+    'New Hires': '',
+    'Quickbooks': '',
+    'Paycor': '',
+    'Policies & Procedures': '',
+    'Performance Reviews': '',
+    'Employee Directory': '',
+  },
+  'projects': {
+    'Client Projects': '',
+    'Client Project Archives': '',
+    'Internal Projects': '',
+    'Internal Project Archives': '',
+  },
+  'sales': {
+    'Public Website': '',
+    'Marketing Collateral': '',
+    'Client Presentations': '',
+    'Case Studies': '',
+    'Ripple Report & Culture Book': '',
+    'Business Development': '',
+    'Salesforce': '',
+  },
+  'operations': {
+    'I.T.': '',
+    'Building/Real Estate': '',
+    'Quickbooks': '',
+    'Expense Reports': '',
+    'Utilization Report': '',
+    'Finance Resources': '',
+  },
+  'forum': {
 
-  ],
+  },
 };
 
 module.exports = ($ => {
-  const topNav = keys(navStruct);
+  const baseUrl = `${window.location.protocol}//${window.location.host}`;
+  const topNavKeys = keys(navStruct);
 
   // For each item in the primary nav bar
-  for (let i = 0; i < topNav.length; ++i) {
-    const navItem = topNav[i];
+  for (let i = 0; i < topNavKeys.length; ++i) {
+    const navItem = topNavKeys[i];
     const navSel = `#nav-${navItem}`;
+
+    const childNavKeys = keys(navStruct[topNavKeys[i]]);
 
     let dropdown = '<ul class="nav-dropdown">\n';
 
     // For each subitem for a primary nav item
-    for (let j = 0; j < navStruct[navItem].length; ++j) {
-      dropdown += `<li class="nav-dropdown-item"><a href="">${navStruct[navItem][j]}</a></li>\n`;
+    for (let j = 0; j < childNavKeys.length; ++j) {
+      let dropdownHref = `${baseUrl}/${topNavKeys[i]}/${navStruct[topNavKeys[i]][childNavKeys[j]]}`;
+      console.log(childNavKeys[j]);
+      dropdown += `<li class="nav-dropdown-item"><a href="${dropdownHref}">${childNavKeys[j]}</a></li>\n`;
     }
 
     dropdown += '</ul>';
 
-    if (navStruct[navItem].length > 0) {
+    if (!isEmpty(navStruct[navItem])) {
       $(navSel).append(dropdown);
     }
-
-    const baseUrl = $(`#nav-${topNav[i]} li > a`).attr('href');
-    console.log(baseUrl);
   }
 })(jQuery);
